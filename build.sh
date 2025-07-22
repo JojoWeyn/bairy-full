8ea0a9474e29c850e48518bc60b3592b56d432e7
damir <jojoweyn@gmail.com>
2025-07-22 17:01:20 +0700

    fix

--- build.sh
+++ build.sh
@@ -2,15 +2,24 @@
 
 echo "📦 Сборка client..."
 cd ./bairy-new
-npm install
-npm run build
+docker build -t client-builder -f client/Dockerfile ./client
 cd ..
 
 echo "📦 Сборка admin..."
 cd ./admin
-npm install
-npm run build
+docker build -t admin-builder -f admin/Dockerfile ./admin
 cd ..
 
+echo "🚢 Создаем контейнеры без запуска"
+#
+docker create --name tmp-client client-builder
+docker cp tmp-client:/app/dist ./nginx/client
+docker rm tmp-client
+
+docker create --name tmp-admin admin-builder
+docker cp tmp-admin:/app/dist ./nginx/admin
+docker rm tmp-admin
+
 echo "🚢 Старт Docker Compose..."
-docker compose up --build
+docker build -t my-nginx -f nginx/Dockerfile ./nginx
+docker run -d -p 80:80 my-nginx
